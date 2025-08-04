ab3e6656b6f117ab9f482a1e1a963501c6330807
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-08-04 04:31:16 +0200

    feat: implemented action type

--- src/action.zig
+++ src/action.zig
@@ -6,9 +6,8 @@ const ResourceCost = struct {
 
 const Action = struct {
     name: []const u8,
-    description: ?[]const u8,
-    cost: ResourceCost,
-    cooldown: f64, // in seconds
-    duration: f64, // in seconds
-    is_available: bool, // whether the action can be performed
-}
+    cost: []const ResourceCost,
+    reward: []const ResourceCost,
+    cooldown: u32,
+    last_tick_used: u64,
+};
